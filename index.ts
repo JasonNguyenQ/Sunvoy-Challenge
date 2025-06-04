ac488475727480ea711120d2a2f0656c554d30c7
Master-AMW <lgnjasonn@gmail.com>
2025-06-04 09:31:30 -0700

    Used internal APIs rather than automating E2E user interaction

--- index.ts
+++ index.ts
@@ -1,59 +1,135 @@
-const { chromium } = require("playwright")
+const crypt = require("crypto")
 const fs = require("fs")
 
+type Tokens = {
+    access_token: string;
+    apiuser: string;
+    language: string;
+    openId: string;
+    operateId: string;
+    userId: string;
+}
+
+type SignedRequest = Tokens & {
+    payload: string;
+    fullPayload: string;
+    timestamp: number;
+    checkcode: string;
+}
+
 type User = {
-    FirstName: string;
-    LastName: string;
-    Email: string;
-    UserID: string;
+    id: string;
+    firstName: string;
+    lastName: string;
+    email: string;
 }
 
-async function fetchCurrentUser(page): Promise<User>{
-    await page.goto("https://challenge.sunvoy.com/settings")
-    await page.waitForSelector("form")
-    const attributes = await page.locator("form input").all()
-    const user: User = {
-        FirstName: await attributes.at(1).getAttribute("value"),
-        LastName: await attributes.at(2).getAttribute("value"),
-        Email: await attributes.at(3).getAttribute("value"),
-        UserID: await attributes.at(0).getAttribute("value"),
-    }
-    return user
-}
-
-async function fetchUsers(page): Promise<User[]>{
-    await page.waitForSelector("#userList > div")
-    const userList = await page.locator("#userList > div").all()
-    
-    const users: User[] = []
-    for(const user of userList){
-        const fullName = (await user.locator("h3").textContent()).split(" ")
-        const userObj: User = {
-            FirstName : fullName[0],
-            LastName : fullName[1],
-            Email : await user.locator("p").first().textContent(),
-            UserID : (await user.locator("p").last().textContent()).replace("ID: ", "")
+async function readStream(stream){
+    const html : String[] = []
+    for await (const chunk of stream) {
+        for (const n of chunk) {
+            html.push(String.fromCharCode(n))
         }
-        users.push(userObj)
     }
+
+    return html.join("")
+}
+
+async function getNonce() : Promise<string>{
+    const response : Response = await fetch('https://challenge.sunvoy.com/login')
+    const html : string = await readStream(response.body)
+    const pattern : string = "<input type=\"hidden\" name=\"nonce\" value=\""
+    const start : number = html.indexOf(pattern)+pattern.length
+    const value : string = html.slice(start,html.indexOf("\"", start))
+    return value
+} 
+
+async function Login(value : string) : Promise<string[]> {
+    const response : Response = await fetch('https://challenge.sunvoy.com/login', {
+        method: "POST",
+        headers: {
+            "Accept": "text/html",
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({nonce: value, username: "demo@example.org", password: "test"}),
+        redirect: "manual"
+    })
+
+    return response.headers.getSetCookie()
+}
+
+async function getUsers(cookies: string[]) : Promise<User[]>{
+    const response : Response = await fetch('https://challenge.sunvoy.com/api/users', {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Cookie": cookies.join(";")
+        },
+    })
+    const users = await response.json()
     return users
 }
 
-async function UserLogin(page): Promise<void> {
-    await page.goto("https://challenge.sunvoy.com")
+function getInputValues(html) : Tokens{
+    const m = {}
+    const patternID : string = "<input type=\"hidden\" id=\""
+    const patternValue : string = "value=\""
+    let s : number = 0
+    while(true){
+        const startID : number = html.indexOf(patternID, s)+patternID.length
+        const id : string = html.slice(startID,html.indexOf("\"", startID))
 
-    await page.locator("input[name=username]").fill("demo@example.org")
-    await page.locator("input[name=password]").fill("test")
-    await page.locator("button[type=submit]").click()
+        const startValue : number = html.indexOf(patternValue, s)+patternValue.length
+        const value : string = html.slice(startValue,html.indexOf("\"", startValue))
+
+        if(startValue <= startID) break
+        m[id] = value
+        s = startValue
+    }
+    return m as Tokens
+}
+
+async function getTokens(cookies: string[]) : Promise<Tokens> {
+    const response : Response = await fetch("https://challenge.sunvoy.com/settings/tokens",{
+        method: "GET",
+        headers: {
+            "Cookie": cookies.join(";")
+        }
+    })
+    return getInputValues(await readStream(response.body))
+}
+
+function createSignedRequest(obj : Tokens) : SignedRequest {
+    const hash = crypt.createHmac("sha1", "mys3cr3t")
+    const timestamp : number = Math.floor(Date.now() / 1e3)
+    const newObj = {...obj, timestamp: timestamp.toString()}
+    const payload : string = Object.keys(newObj).sort().map(t => `${t}=${encodeURIComponent(obj[t])}`).join("&")
+    hash.update(payload)
+    const checkcode = hash.digest("hex").toUpperCase()
+    const fullPayload : string = `${payload}&checkcode=${checkcode}`
+    return {payload, checkcode, timestamp, fullPayload} as SignedRequest
+}
+
+async function getCurrentUser(cookies : string[]) : Promise<User>{
+    const tokens : Tokens = await getTokens(cookies)
+    const signedRequest : SignedRequest = createSignedRequest(tokens)
+    const response : Response = await fetch("https://api.challenge.sunvoy.com/api/settings",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-www-form-urlencoded"
+        },
+        body: signedRequest.fullPayload
+    });
+    return await response.json()
 }
 
 async function main(){
-    const browser = await chromium.launch({headless: true})
-    const page = await browser.newPage()
-    await UserLogin(page)
-    const users: User[] = [...(await fetchUsers(page)), await fetchCurrentUser(page)]
-    fs.writeFile("users.json", JSON.stringify(users), ()=>{})
-    browser.close()
+    const nonce : string = await getNonce()
+    const cookies : string[] = await Login(nonce)
+    const users : User[] = await getUsers(cookies)
+    const currentUser : User = await getCurrentUser(cookies)
+    const combinedUsers : User[] = [...users, currentUser]
+    fs.writeFile("users.json", JSON.stringify(combinedUsers), ()=>{})
 }
 
 main()
